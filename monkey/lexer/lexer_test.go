7f84f1468b38e7d17b800ad8046a1fa54cba7f5f
Thomas Ferro <t.ferro184@gmail.com>
2020-08-31 11:02:03 +0200

    Finishing lexer with EQ and NOT_EQ tokens

--- monkey/lexer/lexer_test.go
+++ monkey/lexer/lexer_test.go
@@ -9,7 +9,9 @@ import (
 func TestNextToken(t *testing.T) {
 	input := `=+(){},;
 	!-/*5;
-	5 < 10 > 5;`
+	5 < 10 > 5;
+	10 == 10;
+	10 != 9;`
 
 	tests := []struct {
 		expectedType    token.TokenType
@@ -34,6 +36,14 @@ func TestNextToken(t *testing.T) {
 		{token.INT, "10"},
 		{token.GT, ">"},
 		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.INT, "10"},
+		{token.EQ, "=="},
+		{token.INT, "10"},
+		{token.SEMICOLON, ";"},
+		{token.INT, "10"},
+		{token.NOT_EQ, "!="},
+		{token.INT, "9"},
 		{token.SEMICOLON, ";"},
 		{token.EOF, ""},
 	}
@@ -64,6 +74,12 @@ func TestMonkeyCode(t *testing.T) {
 	};
 	
 	let result = add(five, ten);
+
+	if (5 < 10) {
+		return true;
+	} else {
+		return false;
+	}
 	`
 
 	tests := []struct {
@@ -106,6 +122,23 @@ func TestMonkeyCode(t *testing.T) {
 		{token.IDENT, "ten"},
 		{token.RPAREN, ")"},
 		{token.SEMICOLON, ";"},
+		{token.IF, "if"},
+		{token.LPAREN, "("},
+		{token.INT, "5"},
+		{token.LT, "<"},
+		{token.INT, "10"},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.RETURN, "return"},
+		{token.TRUE, "true"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
+		{token.ELSE, "else"},
+		{token.LBRACE, "{"},
+		{token.RETURN, "return"},
+		{token.FALSE, "false"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
 		{token.EOF, ""},
 	}
 
