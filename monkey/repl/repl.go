acbbdc21c90f4369f9f31bb18a9d47c4fee109a2
Thomas Ferro <t.ferro184@gmail.com>
2020-09-04 11:30:25 +0200

    Evaluator: identifiers

--- monkey/repl/repl.go
+++ monkey/repl/repl.go
@@ -3,6 +3,7 @@ package repl
 import (
 	"ThomasFerro/monkey/evaluator"
 	"ThomasFerro/monkey/lexer"
+	"ThomasFerro/monkey/object"
 	"ThomasFerro/monkey/parser"
 	"bufio"
 	"fmt"
@@ -13,6 +14,7 @@ const PROMPT = ">> "
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
+	env := object.NewEnvironment()
 
 	for {
 		fmt.Fprintf(out, PROMPT)
@@ -32,7 +34,7 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		evaluated := evaluator.Eval(program)
+		evaluated := evaluator.Eval(program, env)
 
 		if evaluated != nil {
 			io.WriteString(out, evaluated.Inspect())
